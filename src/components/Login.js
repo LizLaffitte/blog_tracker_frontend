6833bf1348961e2323bcc50ee52644da47c456d0
lizlaffitte <laffitte.digital@gmail.com>
2021-03-01 18:36:03 -0500

    Login component implementation.

---
+++ src/components/Login.js
@@ -0,0 +1,32 @@
+import React, { useState} from "react"
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import {auth} from '../actions/userActions'
+
+function Login() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const history = useHistory()
+    const dispatch = useDispatch()
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        dispatch(auth({username, password}))  
+        // history.push('/:username')    
+    }
+
+    return(
+        <>
+        <form onSubmit={handleOnSubmit}>
+            <h1>Log in</h1>
+            <label>Username: </label><br />
+            <input type="text" value={username} onChange={e => setUsername(e.target.value)} name="username" /><br /><br />
+            <label>Password: </label> <br />
+            <input type="password" value={password} name="password" onChange={e => setPassword(e.target.value)} />
+            <br /><br />
+            <input type="submit" value="Log in"  />
+        </form>
+        </>
+    )
+}
+
+export default Login
