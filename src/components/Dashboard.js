3f51ac5645fb56fca1bdcecc2e8eeb3e7f393186
lizlaffitte <laffitte.digital@gmail.com>
2021-03-01 18:35:18 -0500

    Stubbing out Dashboard component.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+// function Dashboard(){
+//     const user = useSelector(state => state.currentUser)
+//     return(
+//         <>
+        
+//         </>
+//     )
+// }
+
+// export default Dashboard
