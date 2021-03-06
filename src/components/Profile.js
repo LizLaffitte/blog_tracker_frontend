60bad3f08e163a90d2c22805aa1ea987e5068b12
lizlaffitte <laffitte.digital@gmail.com>
2021-03-06 13:16:07 -0500

    Stubbing out Profile component to display user details and provide place to add API key for AirTable.

---
+++ src/components/Profile.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {useSelector} from "react-redux";
+import {Link} from 'react-router-dom';
+
+function Profile(){
+    
+    const user = useSelector(state => state.user)
+    const token = useSelector(state => state.token)
+    if(user){
+        console.log(user.username)
+    }
+    return(
+        <>
+        <h1>{user.username}'s Profile</h1>
+        <table>
+            <tr>
+                <td>Username:</td>
+                <td>{user.username}</td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td>{user.email}</td>
+            </tr>
+            <tr>
+                <td>AirTable API Key:</td>
+                <td>{user.at_key || <Link to="">Add AirTable API Key</Link>}</td>
+            </tr>
+        </table>
+        </>
+    )
+}
+export default Profile
