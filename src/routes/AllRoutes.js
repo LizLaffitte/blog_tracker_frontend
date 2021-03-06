7f2849a45d307ecd807eb73284a40b1f88cb2d63
lizlaffitte <laffitte.digital@gmail.com>
2021-03-06 13:18:29 -0500

    Add User Profile route to AllRoutes.

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Signup from '../components/Signup'
 import Login from '../components/Login'
-// import Dashboard from '../components/Dashboard'
+import Profile from '../components/Profile'
 function AllRoutes() {
-
     return(
         <>
             <Router>
                 <Switch>
                     <Route exact path='/signup' component={Signup} />
                     <Route exact path='/login' component={Login} />
-                    {/* <Route exact path='/:username' component={Dashboard} /> */}
+                    <Route exact path='/:username' component={Profile} />
                 </Switch>
             </Router>
         </>
